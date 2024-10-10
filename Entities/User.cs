d0758041bade6a55dd04728ce058c33f2051821c
RachuSharma <147827045+RachuSharma@users.noreply.github.com>
2024-10-10 10:23:49 +0200

    Initial push

---
+++ Entities/User.cs
@@ -0,0 +1,13 @@
+﻿namespace Entities;
+
+public class User
+{
+    public string UserName { get; set; }
+    public string password { get; set; }
+    public int id { get; set; }
+
+    public User()
+    {
+        
+    }
+}
